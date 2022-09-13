d0611920e03dd27d653c9a3f7c7194454c2843ef
geetNaina <gp@esokia-webagency.com>
2022-09-13 15:57:00 +0400

    complete

--- js/script.js
+++ js/script.js
@@ -2,7 +2,5 @@ $(document).ready(function(){
     $('.icon').on('click', function(){
         $('nav ul').toggleClass('showing');
     });
-    $('nav').on('mouseleave', function(){
-        $('nav ul').toggleClass('showing');
-  });
+ 
 });
